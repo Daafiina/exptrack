9446d1e6c7b0624c3a2d8f0971b5b28bbe223b67
Dafina Aliji <da52811@ubt-uni.net>
2025-10-07 23:56:31 +0200

    Add App.jsx with main component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,144 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import Toast from "./components/Toast";
+import Header from "./components/Header";
+import DataCard from "./components/DataCard";
+import ExpenseInputForm from "./components/ExpenseInputForm";
+import FilterMenu from "./components/FilterMenu";
+import ExpensesList from "./components/ExpensesList";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [formData, setFormData] = useState({
+    description: "",
+    amount: "",
+    category: "",
+    date: "",
+    type: "income",
+  });
+
+  const [editingId, setIsEditingId] = useState(null);
+  const [expenses, setExpenses] = useState([]);
+  const [filter, setFilter] = useState("all");
+  const [toast, setToasts] = useState([]);
+
+  useEffect(() => {
+    const savedExpenses = JSON.parse(localStorage.getItem("expenses"));
+    if (savedExpenses) {
+      setExpenses(savedExpenses);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (expenses.length > 0) {
+      localStorage.setItem("expenses", JSON.stringify(expenses));
+    }
+  }, [expenses]);
+
+  //Toast nottification
+
+  const showToast = (message, type = "succes") => {
+    const id = Date.now();
+    const toast = { id, message, type };
+    setToasts((prev) => [...prev, toast]);
+    setTimeout(() => {
+      setToasts((prev) => prev.filter((t) => t.id !== id));
+    }, 4000);
+  };
+
+  const RemoveToast = (id) => {
+    setToasts((prev) => prev.filter((t) => t.id !== id));
+  };
+
+  //Kalkulimi Total Income
+  const totalIncome = expenses
+    .filter((exp) => exp.type === "income")
+    .reduce((acc, exp) => acc + parseFloat(exp.amount), 0);
+
+  //Total income
+
+  const totalExpenses = expenses
+    .filter((exp) => exp.type === "expense")
+    .reduce((acc, exp) => acc + parseFloat(exp.amount), 0);
+
+  const balance = totalIncome - totalExpenses;
+
+  //Filter expense
+
+  const filteredExpenses = expenses.filter((exp) => {
+    if (filter === "all") return true;
+    return exp.type === filter;
+  });
+
+  const handleEdit = (expense) => {
+    setFormData({
+      description: expense.description,
+      amount: expense.amount.toString(),
+      category: expense.category,
+      date: expense.date,
+      type: expense.type,
+    });
+    setIsEditingId(expense.id);
+    showToast("Entry loaded for editing", "info");
+  };
+
+  const handleDelete = (id) => {
+    const expensetoDelete = expenses.find((exp) => exp.id === id);
+    setExpenses(expenses.filter((exp) => exp.id !== id));
+    showToast(
+      `${
+        expensetoDelete?.type === "income" ? "Income" : "Expense"
+      } deleted succesfully `,
+      "error"
+    );
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-indigo-900 to-black p-4">
+      {/* Toast */}
 
-export default App
+      <Toast Toast={toast} RemoveToast={RemoveToast} />
+
+      {/* Header */}
+
+      <div className="max-w-7xl mx-auto">
+        <Header />
+
+        {/* Data cards */}
+        <DataCard
+          totalIncome={totalIncome}
+          totalExpenses={totalExpenses}
+          balance={balance}
+        />
+
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-8 px-4">
+          {/* Expenses Form */}
+          <div className="md:col-span-2 lg:col-span-2">
+            <ExpenseInputForm
+              formData={formData}
+              setFormData={setFormData}
+              editingId={editingId}
+              setIsEditingId={setIsEditingId}
+              expenses={expenses}
+              setExpenses={setExpenses}
+              showToast={showToast}
+            />
+          </div>
+          <div className="md:col-span-3 lg:col-span-3 flex justify-center ">
+            <div
+              className="bg-white/5 backdrop-blur-lg rounded-3xl border
+            border-white/10 shadow-2xl overflo-hidden w-full max-w-2xl"
+            >
+              <FilterMenu filter={filter} setFilter={setFilter} />
+
+              {/* Expenses List */}
+              <ExpensesList
+                filteredExpenses={filteredExpenses}
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
