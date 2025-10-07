24ec9666fc1c5e967f7020bfe2509fd9184ddb15
Dafina Aliji <da52811@ubt-uni.net>
2025-10-08 00:12:11 +0200

    Added categories.js for expense and income categories

---
+++ src/util/categories.js
@@ -0,0 +1,27 @@
+export const expenseCategories = {
+  expense: [
+    "Food & Groceries",
+    "Rent & Utilities",
+    "Transportation",
+    "Health & Medical",
+    "Entertainment",
+    "Shopping",
+    "Education",
+    "Insurance",
+    "Travel",
+    "Other",
+  ],
+
+  income: [
+    "Salary",
+    "Business",
+    "Investments",
+    "Freelance",
+    "Rental Income",
+    "Gifts",
+    "Interest",
+    "Dividends",
+    "Refunds",
+    "Other",
+  ],
+};
